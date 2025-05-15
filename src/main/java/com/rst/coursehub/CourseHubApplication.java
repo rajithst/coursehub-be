33817d6ff7c73be243bae31b9ea93919a15839a8
rajithst <rajiththennakoonshady@gmail.com>
2025-05-15 23:20:33 +0900

    Initial commit for CourseHub Backend

---
+++ src/main/java/com/rst/coursehub/CourseHubApplication.java
@@ -0,0 +1,13 @@
+package com.rst.coursehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CourseHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rst/coursehub/CourseHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rst.coursehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
